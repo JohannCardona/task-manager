64ffc61d3aa79ea662c184b066de72f90ff54e86
Johann Cardona <2007048@brunel.ac.uk>
2026-04-23 15:18:11 +0100

    feat(frontend): wrap app with ToastProvider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
+import { ToastProvider } from './context/ToastContext'
 import ProtectedRoute from './components/ProtectedRoute'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
@@ -9,6 +10,7 @@ export default function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
+      <ToastProvider>
         <Routes>
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
@@ -22,6 +24,7 @@ export default function App() {
           />
           <Route path="*" element={<Navigate to="/tasks" replace />} />
         </Routes>
+      </ToastProvider>
       </AuthProvider>
     </BrowserRouter>
   )
