d9a1c666f2a48fce0bca4de56f6634e31deb757c
Johann Cardona <2007048@brunel.ac.uk>
2026-05-07 19:19:24 +0100

    Update database.py

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -3,9 +3,10 @@ from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 from app.core.config import settings
 
+is_sqlite = settings.database_url.startswith("sqlite")
 engine = create_engine(
     settings.database_url,
-    connect_args={"check_same_thread": False},  # needed for SQLite
+    connect_args={"check_same_thread": False} if is_sqlite else {},
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
